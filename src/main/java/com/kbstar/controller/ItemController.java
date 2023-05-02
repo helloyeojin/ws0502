faec419373ac332fa890a5b8b7de1cabba15e346
helloyeojin <bonuibomb@gmail.com>
2023-05-02 16:51:05 +0900

    ws0502

---
+++ src/main/java/com/kbstar/controller/ItemController.java
@@ -0,0 +1,18 @@
+package com.kbstar.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/items")
+public class ItemController {
+
+}
